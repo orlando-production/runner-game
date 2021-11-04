9abcb099a308df8900db453f84621e0400ab7ea8
Korotkov Daniil <37455205+danythere@users.noreply.github.com>
2021-11-04 17:14:35 +0300

    https://trello.com/c/x2HLmSjs (#1)
    * https://trello.com/c/x2mjs
    
    * file-loader, postcss
    
    * file-loader, postcss
    
    Co-authored-by: unknown <danythere@yandex.ru>

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import orlandoImg from "./../assets/orlando.jpg";
+
+export function App() {
+  return (
+    <div>
+      <h1>I ❤️ Orlando!</h1>
+      <img src={orlandoImg} />
+    </div>
+  );
+}
---
+++ webpack.prod.config.ts
@@ -0,0 +1,70 @@
+import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import "webpack-dev-server";
+
+const config: Configuration = {
+  mode: "production",
+  output: {
+    publicPath: "/",
+    clean: true,
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "static/index.html",
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+  devtool: false,
+};
+
+export default config;
