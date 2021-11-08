185e0655ba04d19e8ec5b05898152f0ddc63053e
s.ivanov <sergey.ivanov@karuna.group>
2021-11-08 12:31:12 +0300

    add leaderboard page

--- src/pages/leaderboard/LeaderboardPage.tsx
+++ src/pages/leaderboard/LeaderboardPage.tsx
@@ -1,33 +1,57 @@
 import {
-    Box, Divider, Link, Avatar, Typography
+    Box, Avatar, Typography
 } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from "../../components/footer";
 import styles from './LeaderboardPage.module.css';
 
 const LeaderboardPage = () => {
+    const [data, setData] = useState([]);
+
+    const plugs = [
+        {nick: 'Volodya777', avatar: '', position: '10', scores: '1000'},
+        {nick: 'Volodya777', avatar: '', position: '10', scores: '1000'},
+        {nick: 'Volodya777', avatar: '', position: '10', scores: '1000'}];
+
+    useEffect(() => {
+        /*Todo тут получаем список участников*/
+        setData(plugs);
+    }, []);
 
     return (
         <div className={styles['leaderboard-page']}>
             <div className={styles['leaderboard-container']}>
-                <Typography component="h1" variant="h5" className={styles['leaderboard-title']} mb={5}>Leaderbord</Typography>
-                <Box
-                    className={styles['leaderboard-content']}
-                >
+                <Typography component="h1" variant="h5" className={styles['leaderboard-title']}
+                            mb={5}>Leaderbord</Typography>
+                <Box className={styles['leaderboard-content']}>
                     <div className={styles['leaderboard-field']}>
-                        <span>Name</span>
-                        <span>Positions</span>
-                        <span>Scores</span>
+                        <Typography color="text.secondary" variant="subtitle1">
+                            Name
+                        </Typography>
+                        <Typography color="text.secondary" variant="subtitle1">
+                            Positions
+                        </Typography>
+                        <Typography color="text.secondary" variant="subtitle1">
+                            Scores
+                        </Typography>
                     </div>
 
-                    <div className={styles['leaderboard-field']}>
-                        <div className={styles['leaderboard-name']}>
-                            <Avatar src="/broken-image.jpg" />
-                            <span>nickname</span>
+                    {data.map((field, index) => (
+                        <div key={index} className={styles['leaderboard-field']}>
+                            <div className={styles['leaderboard-name']}>
+                                <Avatar src={field.avatar}/>
+                                <Typography sx={{ml: 2}} color="text.secondary" variant="body1">
+                                    {field.nick}
+                                </Typography>
+                            </div>
+                            <Typography color="text.secondary" variant="body1">
+                                {field.position}
+                            </Typography>
+                            <Typography color="text.secondary" variant="body1">
+                                {field.scores}
+                            </Typography>
                         </div>
-                        <span>1</span>
-                        <span>1</span>
-                    </div>
+                    ))}
 
                 </Box>
             </div>
