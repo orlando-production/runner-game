2eef8347bb7960d3012fc675f2fd791008636dd7
s.ivanov <sergey.ivanov@karuna.group>
2021-11-11 23:12:47 +0300

    add api for profil, add methods apo

--- src/services/RequestData.ts
+++ src/services/RequestData.ts
@@ -3,13 +3,13 @@ import API from '../api/API';
 export const requestPostData = <P, R>(
   url: string,
   params: Record<string, P>
-) => API.post<R>(url, params, {withCredentials: true});
+) => API.post<R>(url, params, { withCredentials: true });
 
-export const requestGetData = <P, R>(
-    url: string,
-) => API.get<R>(url, {withCredentials: true});
+export const requestGetData = <R>(
+  url: string
+) => API.get<R>(url, { withCredentials: true });
 
 export const requestPutData = <P, R>(
-    url: string,
-    params: Record<string, P>
-) => API.put<R>(url, params, {withCredentials: true});
+  url: string,
+  params: Record<string, P>
+) => API.put<R>(url, params, { withCredentials: true });
