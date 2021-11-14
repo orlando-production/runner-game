98356ec21beb0fbc85ae1a6cbde425eaf3567dd5
s.ivanov <sergey.ivanov@karuna.group>
2021-11-14 15:32:24 +0300

    main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,21 +1,36 @@
 /* eslint-disable no-console */
-// import {
-//   Box, Button, TextField, Typography
-// } from '@mui/material';
-import React, { useState } from 'react';
+import {
+    Box, IconButton
+} from '@mui/material';
+import React, {useState} from 'react';
 import Footer from '../../components/footer/Footer';
+import {Icon} from "@iconify/react";
 import styles from './MainPage.module.css';
 
 const MainPage = () => {
 
-  return (
-    <div className={styles['main-page']}>
-      <div className={styles['main-container']}>
-        <h1>Hui</h1>
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <Box className={styles['main-page']}>
+            <Box className={styles['main-container']}>
+                <Box className={styles['icons']}>
+                    <div className={styles['icon-wrap']}>
+                        <Icon icon="mdi:clipboard-account-outline" height={24} color="rgb(var(--icon-color))"/>
+                    </div>
+
+                    <div className={styles['icon-wrap']}>
+                        <Icon icon="mdi:clipboard-account-outline" height={24} color="rgb(var(--icon-color))"/>
+                    </div>
+                </Box>
+                <Box className={styles['block-img']}>
+                    <div>Привет</div>
+                </Box>
+                <Box className={styles['block-content']}>
+                    <div>Привет</div>
+                </Box>
+            </Box>
+            <Footer/>
+        </Box>
+    );
 };
 
 export default MainPage;
