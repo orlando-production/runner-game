5ac2df0cf2feaf6aa93317fe43d09bd9c1005356
s.ivanov <sergey.ivanov@karuna.group>
2021-11-23 23:08:39 +0300

    pr fix

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -21,6 +21,8 @@ type InitialFormData = {
   login: string
 };
 
+type Status = 'invisible' | 'error' | 'success';
+
 const initialFormData = {
   id: '',
   first_name: '',
@@ -34,8 +36,8 @@ const ProfilePage = () => {
   const [avatar, setAvatar] = useState<string>();
   const [oldPassword, setOldPassword] = useState<string>();
   const [newPassword, setNewPassword] = useState<string>();
-  const [status, setStatus] = useState<string>('invisible');
-  const [statusPassword, setStatusPassword] = useState<string>('invisible');
+  const [status, setStatus] = useState<Status>('invisible');
+  const [statusPassword, setStatusPassword] = useState<Status>('invisible');
   const [warningText, setWarningText] = useState<string>('');
   const [warningTextPassword, setWarningTextPassword] = useState<string>('');
   const [formData, updateFormData] = useState<InitialFormData>(initialFormData);
