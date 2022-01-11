f7e787ae08ecec79a3dbfffaa428e8bb4f2e9fc9
s.ivanov <sergey.ivanov@karuna.group>
2022-01-11 19:06:58 +0300

    fix after review

--- src/actions/themes/index.ts
+++ src/actions/themes/index.ts
@@ -1,4 +1,4 @@
-export const FETCH_THEMES = 'auth/SIGNUP';
+export const FETCH_THEMES = 'theme';
 
 export const FETCH_THEMES_PENDING = `${FETCH_THEMES}/pending`;
 export const FETCH_THEMES_FILFILLED = `${FETCH_THEMES}/fulfilled`;
