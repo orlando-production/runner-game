6452ca84db4f47418eb9678023c6b7f736f446e0
Jane2727 <janej91@mail.ru>
2022-01-12 02:28:34 +0300

    forum models

--- src/db/init/index.ts
+++ src/db/init/index.ts
@@ -1,3 +1,4 @@
+import { forumTopicModel, topicMessage } from 'db/models/foumModel';
 import { themeModel } from 'db/models/theme';
 import { themeUserModel } from 'db/models/themeUser';
 import { Sequelize } from 'sequelize';
@@ -23,6 +24,13 @@ Theme.hasOne(ThemeUser, {
   // The possible choices are RESTRICT, CASCADE, NO ACTION, SET DEFAULT and SET NULL
   onDelete: 'RESTRICT', // Default is SET NULL
   onUpdate: 'RESTRICT' // Default is CASCADE
+});
+
+export const ForumTopic = sequelize.define('ForumTopic', forumTopicModel, {});
+export const TopicMessage = sequelize.define('TopicMessage', topicMessage, {});
+
+ForumTopic.hasMany(TopicMessage, {
+  sourceKey: 'id'
 });
 
 export async function dbConnect() {
