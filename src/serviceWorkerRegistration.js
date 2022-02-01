c85147bd518145e51e210b745749beca08648553
unknown <danythere@yandex.ru>
2022-02-01 14:35:20 +0300

    src-sw

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -7,7 +7,7 @@ export default function registerServiceWorker() {
 
   // Check if the serviceWorker Object exists in the navigator object ( means if browser supports SW )
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
+    window.addEventListener('load', () => {
       const wb = new Workbox('./src-sw.js');
 
       wb.addEventListener('fetch', (event) => {
