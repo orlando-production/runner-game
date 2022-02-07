b23ddcab35c27b8181267a8e3bc80cac1c341e07
unknown <danythere@yandex.ru>
2022-02-07 21:24:24 +0300

    linter

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -6,12 +6,9 @@ import {
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Icon } from '@iconify/react';
 import { useHistory } from 'react-router-dom';
 import classNames from 'classnames';
 import { UserResult } from 'services/Profile';
-import { LeaderboardGetResult } from 'services/Leaderboard';
-import { getList } from 'selectors/leaderboard';
 import Footer from '../../components/footer/Footer';
 import styles from './ProfilePage.module.css';
 import commonStyles from '../../components/common.module.css';
